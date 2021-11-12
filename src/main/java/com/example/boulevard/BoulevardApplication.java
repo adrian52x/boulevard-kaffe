36b3525bc14b2184827dacab03d7116ba26f2e0e
Enachi Adrian <enachi.adrian52@gmail.com>
2021-11-12 10:35:43 +0100

    Initial commit

---
+++ src/main/java/com/example/boulevard/BoulevardApplication.java
@@ -0,0 +1,13 @@
+package com.example.boulevard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoulevardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BoulevardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/boulevard/BoulevardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.boulevard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoulevardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
